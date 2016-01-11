c93bc40af80241ab4dd911a2fedf2a95ed46fbc1
dblock <dblock@dblock.org>
2016-01-10 19:27:28 -0500

    Escape help.

--- slack-bot-server/commands/help.rb
+++ slack-bot-server/commands/help.rb
@@ -2,6 +2,7 @@ module SlackBotServer
   module Commands
     class Help < SlackRubyBot::Commands::Base
       HELP = <<-EOS
+```
 I am your friendly slack-bot-server, here to help.
 
 General
@@ -10,6 +11,7 @@ General
 help               - get this helpful message
 whoami             - print your username
 
+```
 EOS
       def self.call(client, data, _match)
         client.say(channel: data.channel, text: [HELP, SlackBotServer::INFO].join("\n"))
