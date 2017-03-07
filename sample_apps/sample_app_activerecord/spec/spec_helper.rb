a89aad0fe8d845a8503b244f2a60c516a8484589
dblock <dblock@dblock.org>
2017-03-07 10:14:05 -0500

    Run tests in the sample apps.

--- sample_apps/sample_app_activerecord/spec/spec_helper.rb
+++ sample_apps/sample_app_activerecord/spec/spec_helper.rb
@@ -2,7 +2,13 @@ $LOAD_PATH.unshift File.expand_path('..', __dir__)
 
 ENV['RACK_ENV'] = 'test'
 
+require 'active_record'
+require 'database_cleaner'
 require 'slack-ruby-bot-server/rspec'
+
+db_config = YAML.load(File.read(File.expand_path('../config/postgresql.yml', __dir__)))[ENV['RACK_ENV']]
+ActiveRecord::Tasks::DatabaseTasks.create(db_config)
+ActiveRecord::Base.establish_connection(db_config)
 
 RSpec.configure do |config|
   config.around :each do |example|
--- sample_apps/sample_app_mongoid/spec/spec_helper.rb
+++ sample_apps/sample_app_mongoid/spec/spec_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift File.expand_path('..', __dir__)
 
 ENV['RACK_ENV'] = 'test'
 
+require 'mongoid'
+require 'database_cleaner'
 require 'slack-ruby-bot-server/rspec'
 
 Mongoid.load!(File.expand_path('../config/mongoid.yml', __dir__), ENV['RACK_ENV'])
