44dc78b06c045e9ec35722c91c2860d3d447fd35
dblock <dblock@dblock.org>
2017-03-07 12:00:10 -0500

    Added API specs.

---
+++ sample_apps/sample_app_activerecord/README.md
@@ -0,0 +1,11 @@
+### What's this?
+
+This is a sample slack-ruby-bot-server with ActiveRecord.
+
+### Run
+
+```
+bundle install
+rake db:create db:migrate
+rackup
+```
--- sample_apps/sample_app_activerecord/db/schema.rb
+++ sample_apps/sample_app_activerecord/db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307164946) do
-
+ActiveRecord::Schema.define(version: 20_170_307_164_946) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "teams", force: :cascade do |t|
-    t.string   "team_id"
-    t.string   "name"
-    t.boolean  "active",     default: true
-    t.string   "domain"
-    t.string   "token"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+  create_table 'teams', force: :cascade do |t|
+    t.string   'team_id'
+    t.string   'name'
+    t.boolean  'active', default: true
+    t.string   'domain'
+    t.string   'token'
+    t.datetime 'created_at',                null: false
+    t.datetime 'updated_at',                null: false
   end
-
 end
