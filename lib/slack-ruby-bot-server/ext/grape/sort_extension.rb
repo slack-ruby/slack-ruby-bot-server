b4f0123c9fca32730e00a5ffc1fd0918621a0f0b
dblock <dblock@dblock.org>
2019-01-17 14:38:15 -0500

    Fix: compatibility with Grape 1.2.x.

--- lib/slack-ruby-bot-server/ext/grape/sort_extension.rb
+++ lib/slack-ruby-bot-server/ext/grape/sort_extension.rb
@@ -1,8 +1,18 @@
 module Grape
   class API
-    def self.sort(value)
-      route_setting :sort, sort: value
-      value
+    module Extensions
+      module SortExtension
+        def sort(value)
+          route_setting :sort, sort: value
+          value
+        end
+      end
     end
   end
+end
+
+if defined? Grape::API::Instance
+  Grape::API::Instance.extend Grape::API::Extensions::SortExtension
+else
+  Grape::API.extend Grape::API::Extensions::SortExtension
 end
