1158b7484c08b7ebc49c3010627dce3b0d7b2a7c
dblock <dblock@dblock.org>
2020-06-16 16:49:15 -0400

    Do not raise errors on purge.

--- lib/slack-ruby-bot-server/models/team/activerecord.rb
+++ lib/slack-ruby-bot-server/models/team/activerecord.rb
@@ -3,11 +3,14 @@ require_relative 'methods'
 class Team < ActiveRecord::Base
   include Methods
 
-  def self.purge!
-    # destroy teams inactive for two weeks
-    Team.where(active: false).where('updated_at <= ?', 2.weeks.ago).each do |team|
-      puts "Destroying #{team}, inactive since #{team.updated_at}, over two weeks ago."
-      team.destroy
+  def self.purge!(dt = 2.weeks.ago)
+    Team.where(active: false).where('updated_at <= ?', dt).each do |team|
+      begin
+        logger.info "Destroying #{team}, inactive since #{team.updated_at}."
+        team.destroy
+      rescue StandardError => e
+        logger.warn "Error destroying #{team}, #{e.message}."
+      end
     end
   end
 end
